a43d5a9071541d35f4242baa9738317e966912a9
Linn Crosetto <linn@hpe.com>
2016-03-04 16:08:24 -0700

    [16/18] acpi: Disable ACPI table override if securelevel is set
    Origin: https://github.com/mjg59/linux/commit/a4a5ed2835e8ea042868b7401dced3f517cafa76
    
    From the kernel documentation (initrd_table_override.txt):
    
      If the ACPI_INITRD_TABLE_OVERRIDE compile option is true, it is possible
      to override nearly any ACPI table provided by the BIOS with an
      instrumented, modified one.
    
    When securelevel is set, the kernel should disallow any unauthenticated
    changes to kernel space. ACPI tables contain code invoked by the kernel, so
    do not allow ACPI tables to be overridden if securelevel is set.
    
    Signed-off-by: Linn Crosetto <linn@hpe.com>
    [bwh: Forward-ported to 4.7: ACPI override code moved to drivers/acpi/tables.c]
    [bwh: Forward-ported to 4.9: adjust context]
    
    Gbp-Pq: Topic features/all/securelevel
    Gbp-Pq: Name acpi-disable-acpi-table-override-if-securelevel-is-s.patch

--- arch/x86/kernel/setup.c
+++ arch/x86/kernel/setup.c
@@ -1173,6 +1173,12 @@ void __init setup_arch(char **cmdline_p)
 	/* Allocate bigger log buffer */
 	setup_log_buf(1);
 
+#ifdef CONFIG_EFI_SECURE_BOOT_SECURELEVEL
+	if (boot_params.secure_boot) {
+		set_securelevel(1);
+	}
+#endif
+
 	reserve_initrd();
 
 	acpi_table_upgrade();
@@ -1180,12 +1186,6 @@ void __init setup_arch(char **cmdline_p)
 	vsmp_init();
 
 	io_delay_init();
-
-#ifdef CONFIG_EFI_SECURE_BOOT_SECURELEVEL
-	if (boot_params.secure_boot) {
-		set_securelevel(1);
-	}
-#endif
 
 	/*
 	 * Parse the ACPI tables for possible boot-time SMP configuration.
